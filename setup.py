f926053137570aec9df5f48fd46724dd20766b84
Sebastián Acuña <sacuna@gmail.com>
2015-08-01 14:21:17 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(name="django-frontflow",
+      version='0.0.1',
+      packages=find_packages(),
+      include_package_data=True,
+      description='Unholster frontend workflow utilities',
+      license='Proprietary',
+      install_requires=[],
+      )
