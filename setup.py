736aaa624e0a4d7f54f52c7d4e727f0f416c7f0f
Sebastián Acuña <sacuna@gmail.com>
2015-09-25 10:53:08 -0300

    Bump version. Ignore dist/*

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="django-frontflow",
-      version='0.0.1',
+      version='0.0.2',
       packages=find_packages(),
       include_package_data=True,
       author='Sebastián Acuña',
